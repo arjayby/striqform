005524379d16564ee4fe276050cad9c019592008
Arjay Bayona <arjayby1@gmail.com>
2023-07-13 07:06:20 +0800

    App theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#0F102B",
+          light: "#0F102B",
+          dark: "#F8FAFD",
+        },
+        secondary: {
+          DEFAULT: "#FFFFFF",
+          light: "#FFFFFF",
+          dark: "#030712",
+        },
+      },
+    },
   },
   plugins: [],
 };
